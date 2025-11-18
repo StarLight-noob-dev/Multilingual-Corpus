ea4204082655bfd35a8fe483428468c7ae7d126b
Nicolas Calderon <nicocalurrutia5@gmail.com>
2025-11-18 16:16:43 +0100

    Move to src/pipeline, added its own logger, refactored field validation and commented functionality

--- src/pipeline/stage/validation/edition_fields.py
+++ src/pipeline/stage/validation/edition_fields.py
@@ -1,15 +1,22 @@
+import logging
 from typing import override, Dict, Any
 
+from src.logger import get_logger
 from src.models.record.edition_record import EditionRecord
 from src.models.results.stage_result import StageResult
 from src.models.results.types import Ok, Err
-from src.stage.context import PipelineContext
-from src.stage.interface import StageInterface
+from src.pipeline.stage.context import PipelineContext
+from src.pipeline.stage.interface import StageInterface
 
+
+logger = get_logger(
+    __name__,
+    handler_level=logging.DEBUG,
+)
 
 class EditionFieldValidation(StageInterface):
     """
-    Performs basic validation on incoming records to ensure they have necessary fields populated for further
+    Performs basic validation on incoming EditionRecords to ensure they have necessary fields populated for further
     processing.
 
     The validation checks include:
@@ -17,7 +24,7 @@ class EditionFieldValidation(StageInterface):
     - Presence of an Open Library ID (`ol_id`).
     - For editions, presence of a non-empty title.
     - If an OCAID (`ocaid`) is present, it must be a non-empty string for retrieval purposes.
-    - Presence of a publishing date.
+    - Presence of a publishing or copyright date.
 
     Records failing these checks are considered invalid and are filtered out. Valid records are passed on to
     subsequent stages.
@@ -41,8 +48,7 @@ class EditionFieldValidation(StageInterface):
                 results.add_ok(Ok(record))
             else:
                 results.add_err(Err(f"Invalid record: missing necessary attributes: {record}"))
-
-        # TODO Add context write file before dispatching to next stage
+        logger.debug(f"{results.summary()}")
         return results
 
     @override
@@ -51,11 +57,14 @@ class EditionFieldValidation(StageInterface):
 
     def _has_necessary_attributes(self, record: EditionRecord) -> bool:
         """Check if the record has necessary attributes for validation."""
-        ol_id = getattr(record, 'ol_id', None)
-        ocaid = getattr(record, 'ocaid', None)
-        title = getattr(record, 'title', None)
-        publishing_date = getattr(record, 'publishing_date', None)
-        return (ol_id is not None and
-                (title is None or (isinstance(title, str) and title.strip() != "")) and
-                (ocaid is None or (isinstance(ocaid, str) and ocaid.strip() != "")) and
-                publishing_date is not None)
+        if not self._validate_string_field(record.id): return False
+        if not self._validate_string_field(record.ocaid): return False
+        if not self._validate_string_field(record.title): return False
+        if record.publishing_date == -1 and record.copyright_date == -1 and len(record.authors) < 1:
+            # If we have no date or authors, copyright validation cannot be performed
+            return False
+        return True
+
+    def _validate_string_field(self, field_value: str | None) -> bool:
+        """Helper method to validate that a string field is non-empty."""
+        return field_value is not None and isinstance(field_value, str) and field_value.strip() != ""
