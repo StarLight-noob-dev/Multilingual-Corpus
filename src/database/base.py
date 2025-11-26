f09557bde3e2fe4de60d73c6cbaf45a8e4e2477f
Nicolas Calderon <nicocalurrutia5@gmail.com>
2025-11-26 13:17:58 +0100

    Refactor EditionRecord to inherit from Base and utilize SQLAlchemy ORM for improved data mapping

--- src/database/base.py
+++ src/database/base.py
@@ -1,3 +1,3 @@
 from sqlalchemy.orm import declarative_base
 
-Base = declarative_base()
+Base = declarative_base() # NOTE: Now all ORM are also dataclasses, upgrade to separated + mappers if needed
