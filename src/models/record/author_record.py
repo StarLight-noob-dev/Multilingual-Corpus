e58cddd38a4dbb038a728e1fd6d56145405c56a1
Nicolas Calderon <nicocalurrutia5@gmail.com>
2025-11-26 12:58:24 +0100

    Refactor AuthorRecord to integrate SQLAlchemy ORM and enhance data attributes

--- src/models/record/author_record.py
+++ src/models/record/author_record.py
@@ -1,16 +1,26 @@
 from dataclasses import dataclass, field
 from typing import Dict, Any, override, Tuple
 
+from sqlalchemy import String, Integer, Boolean
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.database.orm_mapper import Base
 from src.models.record.interface import IRecord
 
 
 @dataclass
-class AuthorRecord(IRecord):
+class AuthorRecord(Base, IRecord):
     """Class representing an author record."""
-    name: str
-    death_date: int = field(default=-1)
-    is_death_date_exact: bool = field(default=False)
-    _work_count: int = field(default=0)
+
+    __tablename__ = "authors"
+
+    _ol_id: Mapped[str] = mapped_column("id", primary_key=True) # Open Library Identifier
+
+    name: Mapped[str] = mapped_column("name", String, nullable=False)
+    birth_date: Mapped[int] = mapped_column("birth_date", Integer, nullable=False, default=-1)
+    death_date: Mapped[int] = mapped_column("death_date", Integer, nullable=False, default=-1)
+    is_death_date_exact: Mapped[bool] = mapped_column("death_exact", Boolean, default=False)
+    _work_count: Mapped[int] = mapped_column("work_count", Integer, default=0)
 
     def add_work(self, amount: int = 1):
         self._work_count += amount
@@ -40,3 +50,8 @@ class AuthorRecord(IRecord):
             self.is_death_date_exact,
             self._work_count
         )
+
+
+    @override
+    def __str__(self):
+        return f"{self.id} {self.name} {self.death_date} {self.is_death_date_exact}"
