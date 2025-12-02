d08da8e29aa134a13c18758d3320fa3e54060205
Nicolas Calderon <nicocalurrutia5@gmail.com>
2025-12-02 15:10:29 +0100

    Refactor get_many_by_ids to return an empty list instead of None

--- src/database/repositories/base_repository.py
+++ src/database/repositories/base_repository.py
@@ -95,7 +95,7 @@ class GenericRepository(ABC, Generic[T_DOMAIN, T_ORM, ID]):
             return None
         return self.to_domain(orm_entity)
 
-    def get_many_by_ids(self, entity_ids: List[ID]) -> Optional[List[T_DOMAIN]]:
+    def get_many_by_ids(self, entity_ids: List[ID]) -> List[T_DOMAIN]:
         """
         Return multiple entities by their IDs or None if none found.
 
@@ -103,14 +103,12 @@ class GenericRepository(ABC, Generic[T_DOMAIN, T_ORM, ID]):
             entity_ids (List[ID]): List of primary keys of the entities to retrieve.
 
         Returns:
-            Optional[List[T_DOMAIN]]: List of domain model instances if found, else None.
+            List[T_DOMAIN]: List of domain model instances if found, else an empty List.
         """
         if not entity_ids:
-            return None
+            return []
         stmt = select(self.model).where(getattr(self.model, self.pk_name) in entity_ids)
         orm_entities: List[T_ORM] = list(self.session.scalars(stmt).all())
-        if not orm_entities:
-            return None
         return [self.to_domain(e) for e in orm_entities]
 
     def get_all(self) -> List[T_DOMAIN]:
