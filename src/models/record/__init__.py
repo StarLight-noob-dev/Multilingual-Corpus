9210ebdcf2bb24e0b12c5a5f23a0dc5f742a5159
Nicolas Calderon <nicocalurrutia5@gmail.com>
2025-12-28 15:39:34 +0100

    Refactor folders into modules for better management of imports

---
+++ src/models/record/__init__.py
@@ -0,0 +1,6 @@
+from .record_interface import IRecord
+from .transport_record import TransportRecord
+from .author_record import AuthorRecord
+from .edition_record import EditionRecord
+
+__all__ = [IRecord, TransportRecord, AuthorRecord, EditionRecord]
---
+++ src/steps/__init__.py
@@ -0,0 +1,1 @@
+from .base import PipelineStep, BaseFilter, BaseTransformer, BaseAction
