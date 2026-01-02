fc6298f88f91afecd1a84fea87e8bdb6605807bf
Nicolas Calderon <nicocalurrutia5@gmail.com>
2026-01-02 16:21:36 +0100

    Add counter by language for demo

--- src/pipeline/steps/aggregators/util_aggregators.py
+++ src/pipeline/steps/aggregators/util_aggregators.py
@@ -1,5 +1,6 @@
 from typing import Any, override
 
+from src.models.record import EditionRecord
 from src.pipeline.steps import BaseAggregator
 
 
@@ -9,8 +10,23 @@ class CountingAggregator(BaseAggregator):
         self.count = 0
 
     @override
-    def update(self, data: Any) -> Any:
+    def update(self, data: Any) -> None:
         self.count += 1
 
     def get_count(self):
         return self.count
+
+
+class LanguageCounterAggregator(BaseAggregator):
+    def __init__(self):
+        super().__init__()
+
+    @override
+    def update(self, data: EditionRecord) -> None:
+        language = data.languages
+        if language not in self.result:
+            self.result[language] = 0
+        self.result[language] += 1
+
+    def get_language_counts(self):
+        return self.result
