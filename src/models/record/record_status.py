64b4de1a8345d5517fbaf778c7a28ded42532798
Nicolas Calderon <nicocalurrutia5@gmail.com>
2026-02-04 12:57:41 +0100

    Updated record_status.py for integration with ACL 2025

--- src/models/record/record_status.py
+++ src/models/record/record_status.py
@@ -1,14 +1,16 @@
 from enum import Enum
 
 
-class RecordStatus(str, Enum):
+class RecordStatus(int, Enum):
     """
     Available statuses for records that are being processed.
     Any record starts as PENDING, then moves to COMPLETED when done,
     or to ERROR if something went wrong. OMIT can be used to mark records
     that should be skipped.
     """
-    PENDING = "pending"
-    COMPLETED = "completed"
-    OMIT = "OMIT"
-    ERROR = "error"
+    PENDING = 0
+    METADATA_EXTRACTED = 1 # First pass from current OL dumps, including download of books when possible.
+    RAG_COMPLETED = 2      # RAG completed; Obtained snippet answers for the record.
+    CONSENSUS_REACHED = 3  # ACL.2025 Median year calculation done.
+    COMPLETED = 4
+    ERROR = -1
